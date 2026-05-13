db321487027320b3e24e0caa17dd9342fb978824
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2026-05-13 18:29:42 +0100

    shortcircuit /.well-known requests

--- apps/xyz/server.js
+++ apps/xyz/server.js
@@ -75,6 +75,11 @@ router.use(limiter);
 
 // redirect if dir is missing in url path.
 router.use((req, res, next) => {
+
+  if (/(?<=\/.well-known\/appspecific)/.test(req.url)) {
+    return;
+  }
+
   if (xyzEnv.DIR && req.url.length === 1) {
     res.setHeader('location', `${xyzEnv.DIR}`);
     return res.status(302).send();
